dd5209164bc77011253493e014b6fc061c4cd85d
Sean Gammage <sean.gammage@omnifederal.com>
2023-08-14 20:35:12 -0500

    Adding first 2 tests

--- quiz/src/main/java/com/problem_solver/quiz/ProblemSolving.java
+++ quiz/src/main/java/com/problem_solver/quiz/ProblemSolving.java
@@ -1,8 +1,46 @@
 package com.problem_solver.quiz;
 
+
 public class ProblemSolving {
 
     private ProblemSolving() {}
 
+    // 1.Sum of Array Elements
+    public static int sumOfArrayElements(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        // for (int num : numbers) {
+        //     sum += num;
+        // }
+        return sum;
+    }
+
+    // 2. Even Numbers between 1 and 20
+    public static String evenNumbersBetween1And20() {
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 2; i <= 20; i +=2) {
+            result.append(i).append(" ");
+        }
+        return "Even numbers between 1 and 20: " + result.toString();
+    }
+
+    // 3. Prime Number Check
+
+    // 4. Factorial Calculation
+
+    // 5. Fibonacci Sequence
+
+    // 6. Maximum Element in an Array
+
+    // 7. Reversing an Array
+
+    // 8. Palindrome Array Check
+
+    // 9. Smallest Element in an Array
+
+    // 10. Sum of Even and Product of Odd Numbers in an Array
 
 }
--- quiz/src/test/java/com/problem_solver/quiz/ProblemSolvingTest.java
+++ quiz/src/test/java/com/problem_solver/quiz/ProblemSolvingTest.java
@@ -18,52 +18,52 @@ public class ProblemSolvingTest {
         assertEquals(expectedOutput, ProblemSolving.evenNumbersBetween1And20());
     }
 
-    @Test
-    public void testIsPrime() {
-        assertTrue(ProblemSolving.isPrime(7));
-        assertFalse(ProblemSolving.isPrime(10));
-    }
+    // @Test
+    // public void testIsPrime() {
+    //     assertTrue(ProblemSolving.isPrime(7));
+    //     assertFalse(ProblemSolving.isPrime(10));
+    // }
 
-    @Test
-    public void testFactorial() {
-        assertEquals(120, ProblemSolving.factorial(5));
-    }
+    // @Test
+    // public void testFactorial() {
+    //     assertEquals(120, ProblemSolving.factorial(5));
+    // }
 
-    @Test
-    public void testFibonacciSequence() {
-        int[] expectedSequence = { 0, 1, 1, 2, 3 };
-        assertArrayEquals(expectedSequence, ProblemSolving.fibonacciSequence(5));
-    }
+    // @Test
+    // public void testFibonacciSequence() {
+    //     int[] expectedSequence = { 0, 1, 1, 2, 3 };
+    //     assertArrayEquals(expectedSequence, ProblemSolving.fibonacciSequence(5));
+    // }
 
-    @Test
-    public void testMaximumElement() {
-        int[] data = { 12, 45, 9, 27, 6 };
-        assertEquals(45, ProblemSolving.maximumElement(data));
-    }
+    // @Test
+    // public void testMaximumElement() {
+    //     int[] data = { 12, 45, 9, 27, 6 };
+    //     assertEquals(45, ProblemSolving.maximumElement(data));
+    // }
 
-    @Test
-    public void testReverseArray() {
-        int[] originalArray = { 1, 2, 3, 4, 5 };
-        int[] expectedReversed = { 5, 4, 3, 2, 1 };
-        assertArrayEquals(expectedReversed, ProblemSolving.reverseArray(originalArray));
-    }
+    // @Test
+    // public void testReverseArray() {
+    //     int[] originalArray = { 1, 2, 3, 4, 5 };
+    //     int[] expectedReversed = { 5, 4, 3, 2, 1 };
+    //     assertArrayEquals(expectedReversed, ProblemSolving.reverseArray(originalArray));
+    // }
 
-    @Test
-    public void testIsPalindrome() {
-        int[] palindromeArray = { 1, 2, 3, 2, 1 };
-        assertTrue(ProblemSolving.isPalindrome(palindromeArray));
-    }
+    // @Test
+    // public void testIsPalindrome() {
+    //     int[] palindromeArray = { 1, 2, 3, 2, 1 };
+    //     assertTrue(ProblemSolving.isPalindrome(palindromeArray));
+    // }
 
-    @Test
-    public void testSmallestElement() {
-        int[] values = { 8, 3, 11, 5, 7 };
-        assertEquals(3, ProblemSolving.smallestElement(values));
-    }
+    // @Test
+    // public void testSmallestElement() {
+    //     int[] values = { 8, 3, 11, 5, 7 };
+    //     assertEquals(3, ProblemSolving.smallestElement(values));
+    // }
 
-    @Test
-    public void testSumOfEvenAndProductOfOdd() {
-        int[] nums = { 2, 5, 7, 10, 3 };
-        assertEquals(12, ProblemSolving.sumOfEvenNumbers(nums));
-        assertEquals(105, ProblemSolving.productOfOddNumbers(nums));
-    }
+    // @Test
+    // public void testSumOfEvenAndProductOfOdd() {
+    //     int[] nums = { 2, 5, 7, 10, 3 };
+    //     assertEquals(12, ProblemSolving.sumOfEvenNumbers(nums));
+    //     assertEquals(105, ProblemSolving.productOfOddNumbers(nums));
+    // }
 }
